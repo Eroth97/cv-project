e168d17dc89b556a500c30ab2142dd89ef0aaa64
David Tellez <davidctellezg@gmail.com>
2022-11-23 13:23:10 -0500

    Create dynamic displaying of the General Info form

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,39 +3,86 @@ import React, {Component} from 'react';
 class GeneralInfo extends Component{
   constructor(props){
     super(props);
-    this.handle = this.handle.bind(this);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: 0,
+    }
+    this.handleSubmission = this.handleSubmission.bind(this);
+    this.changeFirstName = this.changeFirstName.bind(this);
+    this.changeLastName = this.changeLastName.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changeNumber = this.changeNumber.bind(this);
   }
 
-  handle(event){
+  changeFirstName(event){
+    this.setState({
+      firstName: event.target.value,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      phone: this.state.phone,
+    })
+  }
+
+  changeLastName(event){
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: event.target.value,
+      email: this.state.email,
+      phone: this.state.phone,
+    })
+  }
+
+  changeEmail(event){
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: event.target.value,
+      phone: this.state.phone,
+    })
+  }
+
+  changeNumber(event){
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      phone: event.target.value,
+    })  
+  }
+
+  handleSubmission(event){
     event.preventDefault();
-    console.log(event.target);
+    console.log(this.state);
   }
 
   render(){
     return(<div className='column'>
       <h2>General Information</h2>
-      <form>
+      <form action='' onSubmit={this.handleSubmission}>
         <div className='column'>
-          <label htmlFor='first-Name'>First Name</label>
-          <input id='first-Name' type='text' />
+          <label htmlFor='firstName'>First Name</label>
+          <input name='firstName' type='text' onChange={this.changeFirstName} required />
         </div>
 
         <div className='column'>
           <label htmlFor='last-Name'>Last Name</label>
-          <input id='last-Name' type='text' />
+          <input name='last-Name' type='text' onChange={this.changeLastName} required />
         </div>
 
         <div className='column'>
           <label htmlFor='email'>Email</label>
-          <input id='email' type='email' />
+          <input name='email' type='email' onChange={this.changeEmail} required />
         </div>
 
         <div className='column'>
           <label htmlFor='number'>Phone Number</label>
-          <input id='number' type='number' />
+          <input name='number' type='number' onChange={this.changeNumber} required />
         </div>
+        
         <div id='button1'>
-          <button type="submit" onClick={this.handle}> Submit</button>
+          <button type="submit"> Submit</button>
         </div>
       </form>
     </div>)
